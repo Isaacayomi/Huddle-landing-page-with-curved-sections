385caa30ef3226602d734551ebf305d3a4e9f3c6
Pr.ime <okunlolaa3@outlook.com>
2023-06-13 18:03:22 +0100

    completed the first section of the project using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins"],
+       Opensans : ["OpenSans"],
+      }
+    },
+  },
+  plugins: [],
+}
+
+
